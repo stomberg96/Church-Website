34ecc41cae2d153efa2094cefafa6dc28417dc33
stomberg96 <stomberg9@gmail.com>
2020-10-03 13:33:54 -0400

    updated logo images. Moved nav links to left side of navbar

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
-import logo from '../assets/hcacLogo1.png';
+import logo from '../assets/hcacLogo3.png';
 
 const Styles = styled.div`
   .navbar {
@@ -27,11 +27,11 @@ const Styles = styled.div`
     align-items: center;
   }
   .navbar-brand img {
-    height: 90px;
+    height: 120px;
     margin-left: 55px
   }
   .navbar-nav {
-    margin-right: 90px
+    margin-left: 10px
   }
 `;
 
@@ -46,7 +46,7 @@ export const NavigationBar = () => (
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
+        <Nav className="navLinks">
           <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
           <Nav.Item><Nav.Link href="/about">About</Nav.Link>  </Nav.Item>
           <Nav.Item><Nav.Link href="/news">News</Nav.Link> </Nav.Item>
